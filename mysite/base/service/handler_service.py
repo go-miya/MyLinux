6392c12c8cd8e10178f898a5179f9fbcfcf3ff80
xing <1070143608@qq.com>
2020-09-19 12:09:42 +0800

    x

---
+++ mysite/base/service/handler_service.py
@@ -0,0 +1,15 @@
+import tornado.ioloop
+import threading
+
+
+class HandlerService:
+    def __init__(self):
+        self.ioloop = tornado.ioloop.IOLoop.instance()
+        self.ioloop.service = self
+
+        self.callback_to_main_thread = self.ioloop.add_callback
+        self._thread = threading.Thread(target=self.loop)
+
+    def loop(self):
+        self.ioloop.loop_greenlet.switch()
+
