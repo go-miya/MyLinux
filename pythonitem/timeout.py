6b2ae24a78d69ccfe61f641822ea124ac911e31d
xing <1070143608@qq.com>
2020-11-04 14:13:18 +0800

    x

---
+++ pythonitem/timeout.py
@@ -0,0 +1,40 @@
+import time
+import signal
+
+
+class TimeOutError(Exception):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+
+def timeOut(interval: int, callback: callable):
+    def decorator(func):
+        def handler(signum, frame):
+            raise TimeOutError("Run func timeout")
+
+        def wrapper(*args, **kwargs):
+            try:
+                signal.signal(signal.SIGALRM, handler)
+                signal.alarm(interval)
+                res = func(*args, **kwargs)
+                signal.alarm(0)
+                return res
+            except TimeOutError as e:
+                callback(e)
+        return wrapper
+    return decorator
+
+
+def timeOutCallback(e: TimeOutError):
+    print(e.msg)
+
+
+@timeOut(2, timeOutCallback)
+def task1():
+    print("task1 start")
+    time.sleep(3)
+    print("task1 finished")
+
+
+if __name__ == "__main__":
+    task1()
