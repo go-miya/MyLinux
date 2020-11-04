d36436e68ee175740eb209c4a32d25acd4c7adec
xing <1070143608@qq.com>
2020-11-04 23:14:09 +0800

    xing

--- pythonitem/redisTest/rediscon.py
+++ pythonitem/redisTest/rediscon.py
@@ -7,13 +7,13 @@ import redis
 class RedisPool:
     redisPool = redis.ConnectionPool(host=host, port=port, decode_responses=True)
     pool = None
-	
-	def __new__(cls, *args, **kwargs):
-		if not cls.instance_cache:
-			cls.redisPool = redis.ConnectionPool(host='localhost', port=3306, decode_responses=True)
-			cls.pool = super().__new__(cls)
-		return cls.pool
-	
-	def __init__(self, host='localhost', port=6379):
-		self.conn = redis.Redis(connection_pool=cls.redisPool)
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.pool:
+            cls.redisPool = redis.ConnectionPool(host='localhost', port=3306, decode_responses=True)
+            cls.pool = super().__new__(cls)
+        return cls.pool
+
+    def __init__(self, host='localhost', port=6379):
+        self.conn = redis.Redis(connection_pool=self.redisPool)
 
