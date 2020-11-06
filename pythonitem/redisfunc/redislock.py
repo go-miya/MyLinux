0e9eef722eb67d132cd7812a05432147a49d91ec
xing <1070143608@qq.com>
2020-11-06 12:16:37 +0800

    xing

--- pythonitem/redisfunc/redislock.py
+++ pythonitem/redisfunc/redislock.py
@@ -9,14 +9,14 @@ from redisfunc.rediscon import RedisPool
 
 
 def redis_lock(func):
-	def wrapper(*args, **kwargs):
-		tag = random.randint(1, 1000)  # define a random num
+    def wrapper(*args, **kwargs):
+        tag = random.randint(1, 1000)  # define a random num
         conn = RedisPool().conn
-		if conn.set("conn.lock", str(tag), nx=True, ex=5):
-			func(*args, **kwargs)
-			if conn.get("conn.lock") == str(tag):
-				conn.delete("conn.lock")
-	return wrapper
+        if conn.set("conn.lock", str(tag), nx=True, ex=5):
+            func(*args, **kwargs)
+            if conn.get("conn.lock") == str(tag):
+                conn.delete("conn.lock")
+    return wrapper
 
 
 
