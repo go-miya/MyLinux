039353abf7114ac57859d1b5571b1606f26acf6f
xing <1070143608@qq.com>
2020-12-16 22:52:13 +0800

    x

--- remote_server/server.py
+++ remote_server/server.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 # -*- coding=utf8 -*-
 
+#import socket
+#
+#host = ""
+#port = 9000
+#
+#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#    s.bind((host, port))
+#    s.listen(1)
+#    conn, addr = s.accept()
+#    with conn:
+#        print("Connected by", addr)
+#        while True:
+#            data = conn.recv(1024)
+#            if not data:
+#                break
+#            conn.sendall(data)
+#            print("receive: ", data)
+#
+import errno
+import functools
 import socket
 
-host = ""
-port = 9000
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((host, port))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by", addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
-            print("receive: ", data)
-
+import tornado.ioloop
+from tornado
