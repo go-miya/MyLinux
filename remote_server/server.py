4e6665caf8d2b641c02a79599e010bed212a8c90
xing <1070143608@qq.com>
2020-12-16 22:35:04 +0800

    x

---
+++ remote_server/server.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding=utf8 -*-
+
+import socket
+
+host = ""
+port = 9000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((host, port))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print("Connected by", addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+            print("receive: ", data)
+
